f0ac8d2f93cbdb7c574d70edffdaf89c269926b8
huangy02 <443663695@qq.com>
2018-03-15 15:43:45 +0800

    swoole之websocket

--- frontend/views/home/comment_client.php
+++ frontend/views/home/comment_client.php
@@ -6,6 +6,22 @@
  * Time: 15:37
  */
 ?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<?php
+$key = '^huangyu.top&swoole$';
+$uid = isset($_GET['uid']) ? intval($_GET['uid']) : 0;
+$token = md5(md5($uid) . $key);
+?>
+
 <div>
     发送内容：<textarea name="content" id="content" cols="30" rows="10"></textarea><br>
     发送给谁：<input type="text" name="toUid" value="" id="toUid"><br>
@@ -26,7 +42,6 @@
     ws.onclose = function(event) {
         console.log('Client has closed.\n');
     };
-
     function send() {
         var obj = document.getElementById('content');
         var content = obj.value;
@@ -34,3 +49,5 @@
         ws.send('{"event":"alertTip", "toUid": '+toUid+'}');
     }
 </script>
+</body>
+</html>
